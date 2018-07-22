75af1336366acdbdb0d40eb32e5a96804ed6d2ea
Russell Lilljedahl <russell.lilljedahl@gmail.com>
2018-07-22 13:44:17 -0400

    Finished if / else

--- ClassLibrary1/ClassLibrary1/IfElseSwitch.cs
+++ ClassLibrary1/ClassLibrary1/IfElseSwitch.cs
@@ -9,6 +9,37 @@ namespace Tutorial
     class IfElseSwitch
     {
         /**
+         * https://www.geeksforgeeks.org/switch-vs-else/
+         * 
+         * If-else conditional branches are great for variable conditions 
+         * that result into a boolean, whereas switch statements are 
+         * great for fixed data values.
+         * 
+         * A switch statement might prove to be faster than ifs provided 
+         * number of cases are good. If there are only few cases, it 
+         * might not effect the speed in any case. Prefer switch if the 
+         * number of cases are more than 5 otherwise, you may use if-else 
+         * too. If a switch contains more than five items, it’s 
+         * implemented using a lookup table or a hash list. This means 
+         * that all items get the same access time, compared to a list of 
+         * if:s where the last item takes much more time to reach as it 
+         * has to evaluate every previous condition first.
+         * 
+         * When compiler compiles a switch statement, it will inspect each 
+         * of the case constants and create a “jump table” that it will use 
+         * for selecting the path of execution depending on the value of 
+         * the expression. Therefore, if we need to select among a large 
+         * group of values, a switch statement will run much faster than 
+         * the equivalent logic coded using a sequence of if-elses. The 
+         * compiler can do this because it knows that the case constants 
+         * are all the same type and simply must be compared for equality 
+         * with the switch expression, while in case of if expressions, the 
+         * compiler has no such knowledge.
+         * 
+         * **/
+
+
+        /**
          * IF / ELSE STATEMENTS
          * if (condition) 
          * {
@@ -83,6 +114,53 @@ namespace Tutorial
                 }
             }
         }
+
+        void ReplaceIfElseWithConditionals()
+        {
+            /**
+             * Conditional statements
+             * if value is true, the first statment, else the second statement.
+             * valueToEvaluate ? ifTrue : ifFalse;
+             * **/
+
+            bool result = true;
+            bool solution;
+            if (!result) // if result is not true
+            {
+                solution = true;
+            }
+            else // if result is true
+            {
+                solution = false;
+            }
+            // BECOMES
+            solution = !result ? true : false;
+
+            result = true;
+            bool test = false;
+            string solution2;
+
+            if (!result) // if result is not true
+            {
+                solution2 = "true";
+            }
+            else if (test) // if test is true
+            {
+                solution2 = "false";
+            }
+            else
+            {
+                solution2 = "invalid";
+            }
+            // BECOMES
+            solution2 = !result ? "true" : test ? "false" : "invalid";
+
+        }
+
+        /**
+         * SWITCH STATEMENTS
+         * 
+         * **/
 
         void SwitchStatements()
         {
